fb65030e8fed92dbdeef137e48644622c0368be7
KENMEGNE YOH IVANA MARINA <yohivana237@gmail.com>
2024-12-05 15:09:31 +0100

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
